d0aa90f98c5b189d0b95a01ce96e0b78f145dbb5
electra777 <electra0789@gmail.com>
2024-02-01 15:44:58 +0300

    added InitialPage & other changes

--- src/App.jsx
+++ src/App.jsx
@@ -62,36 +62,6 @@ function App({ getImages, results }) {
 	return getPage(page);
 }
 
-function InitialPage({ onStart }) {
-	const buttons = GAME_TYPES.map(({ type, icon, label }) => (
-		<button
-			className={`ico-button ico-button-${icon}`}
-			key={icon}
-			onClick={() => onStart(type)}
-			type="button">
-			{label}
-		</button>
-	));
-
-	return (
-		<section className="rules container">
-			<h2>Добро пожаловать!</h2>
-			<p>Memory — игра для тренировки визуальной памяти</p>
-			<div className="rules-panel">
-				<h3>Правила игры</h3>
-				<ul className="rules-list">
-					<li>В наборе есть множество карточек – по две штуки с одним и тем же рисунком.</li>
-					<li>
-						Нужно разложить карточки «рубашкой» вверх на столе, а затем переворачивать по две.
-					</li>
-					<li>Если они совпадают – игрок забирает их и получает ещё один ход.</li>
-				</ul>
-			</div>
-			{buttons}
-		</section>
-	);
-}
-
 function GamePage({ images = [], onShowResults, type }) {
 	const { finishedItems, stepsCount, checkItems, isWin } = useGame(images);
 
