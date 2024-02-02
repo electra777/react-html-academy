cfb649f65a4b463b5a0a8331ae1436529803a514
electra777 <electra0789@gmail.com>
2024-02-02 17:28:37 +0300

    added GamePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import getDeclension from '@dubaua/get-declension';
+import InitialPage from './components/InitialPage';
+import GamePage from './components/GamePage';
 
 const useGame = (images) => {
 	const [stepsCount, setStepsCount] = React.useState(0);
@@ -60,30 +62,6 @@ function App({ getImages, results }) {
 		}
 	};
 	return getPage(page);
-}
-
-function GamePage({ images = [], onShowResults, type }) {
-	const { finishedItems, stepsCount, checkItems, isWin } = useGame(images);
-
-	const handleResultsClick = () => {
-		onShowResults(stepsCount);
-	};
-
-	return (
-		<section className="game container">
-			<Progress stepsCount={stepsCount} value={finishedItems.length / 2} max={images.length / 2} />
-			<Grid images={images} checkItems={checkItems} finishedItems={finishedItems} type={type} />
-			{isWin && (
-				<Modal>
-					<h3 className="modal-caption">Победа!</h3>
-					<p className="modal-description">Теперь давайте узнаем результаты этой партии</p>
-					<button onClick={handleResultsClick} className="button modal-button">
-						Показать результаты
-					</button>
-				</Modal>
-			)}
-		</section>
-	);
 }
 
 function ResultsPage({ results, current, onResetGame }) {
---
+++ src/components/GamePage.jsx
@@ -0,0 +1,25 @@
+function GamePage({ images = [], onShowResults, type }) {
+	const { finishedItems, stepsCount, checkItems, isWin } = useGame(images);
+
+	const handleResultsClick = () => {
+		onShowResults(stepsCount);
+	};
+
+	return (
+		<section className="game container">
+			<Progress stepsCount={stepsCount} value={finishedItems.length / 2} max={images.length / 2} />
+			<Grid images={images} checkItems={checkItems} finishedItems={finishedItems} type={type} />
+			{isWin && (
+				<Modal>
+					<h3 className="modal-caption">Победа!</h3>
+					<p className="modal-description">Теперь давайте узнаем результаты этой партии</p>
+					<button onClick={handleResultsClick} className="button modal-button">
+						Показать результаты
+					</button>
+				</Modal>
+			)}
+		</section>
+	);
+}
+
+export default GamePage;
